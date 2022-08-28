008f0e0ce4cbc96201be83c04e3874b01850e0da
Raghuveer-07 <110120267+Raghuveer-07@users.noreply.github.com>
2022-08-29 03:44:54 +0530

    translation app done

--- app.js
+++ app.js
@@ -7,8 +7,30 @@ var outputDiv = document.querySelector("#output");
 //outputDiv.innerText ="Raghuveer Deepala"
 //console.log(txtInput)
 
+//var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("server failed please try again after sometime.")
+}
+
 function clickHandler() {
-    outputDiv.innerText ="Translated : " + txtInput.value; 
+    var inputText = txtInput.value;
+    var URL = constructURL(inputText);
+    fetch(URL)
+    .then(Response => Response.json())
+    .then(json => {
+            var outputText = json.contents.translated;
+            outputDiv.innerText =  outputText;    
+            console.log(json.contents.translated)
+        })
+    .catch(errorHandler)
 };
 
 
